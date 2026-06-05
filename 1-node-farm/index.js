d27f3600442766903e31d16f11aa3dbac140ead3
Fahad Ali <66764194+fahaddevX@users.noreply.github.com>
2026-06-05 17:11:34 +0500

    feat: added async read and write file code example

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,26 @@
 import fs from 'fs';
 
+//Blocking, synchronous way
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(textIn);
 
 const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File written!');
+
+//Non-blocking, asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+    console.log(data);
+});
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+  fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {    
+    console.log(data2 + data3);
+    fs.writeFile('./txt/final.txt', data2 + data3, (err) => {
+      console.log('Your file has been written!');
+    });
+  });
+});
+});
+
+console.log('Will read file!');
